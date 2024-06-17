cf95290af25e6f24748cd70065eb361ae9aacba2
Oleksii Bespalyi <skripkinlexxx@gmail.com>
2024-06-17 14:23:15 +0200

    add file

--- src/js/pixabay-api.js
+++ src/js/pixabay-api.js
@@ -9,17 +9,9 @@ export function getImages(imageName) {
       safesearch: true,
     });
     const url = `${BASE_URL}${END_POINT}?${params}`;
-    return fetch(url).then(res => res.json()).then(data => {
-      const arr = [];
-      if (data.hits === arr) {
-        iziToast.show({
-          message:
-            'Sorry, there are no images matching your search query. Please, try again!',
-          messageColor: '#fafafb',
-        });
-      }
-    }).catch(error => {
-      console.log(error);
-    });
-      
+  
+    return fetch(url)
+      .then(response => response.json())
+      .catch(error => {
+        console.log(error)});
   }
